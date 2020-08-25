99c8cbb12e97c72b7330e1b90a987cc392a0dac4
Michał Martyniak <michal.martyniak@linux.pl>
2020-08-25 13:00:57 +0200

    add polybar

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar -c ~/.config/polybar/fancybar.ini mainbar &
